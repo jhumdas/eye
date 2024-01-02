f28d810aa3f71e99b769d6fd3e1a1121f9f9e8bb
titas2002 <titaspratihar@gmail.com>
2024-01-02 19:37:23 +0530

    up

---
+++ HVAC.php
@@ -0,0 +1,116 @@
+<?php include "includes/header.php"; ?>
+
+<section class="aboutbanner">
+    <div class="aboutBannerImg">
+        <img src="assets/imgaes/aboutBanner.jpg" alt="banner" />
+    </div>
+    <div class="AboutbannerOverlay"></div>
+    <div class="aboutBannerContain">
+        <div class="bannerTitle">
+            <h2>HVAC</h2>
+        </div>
+        <div class="Anoutbannerlink">
+            <a href="home.php">Home <i class="fa-solid fa-angle-right"></i> </a>
+            <a href="HVAC.php">HVAC</a>
+        </div>
+    </div>
+</section>
+
+<section class="aboutcontenSection">
+    <div class="Cust_container">
+        <div class="aboutContain">
+            <div class="aboutTitle">
+                <h5>HVAC</h5>
+            </div>
+            <div class="hvacSection">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="mechanicalCards">
+                            <div class="cardimg">
+                                <img src="assets/imgaes/Rectangle 23796.png" alt="img" />
+                            </div>
+                            <div class="cardTitle">
+                                <h6>Mechanical</h6>
+                            </div>
+                            <div class="cardcontain">
+                                <ul>
+                                    <li>3D drafting drawings</li>
+                                    <li>General Layout</li>
+                                    <li>General Assembly</li>
+                                    <li>Detailed component drawing</li>
+                                    <li>Detailed fabrication drawing</li>
+                                    <li>Converting 3D to 2D drawing</li>
+                                    <li>Staging and Assembly drawings</li>
+                                </ul>
+                            </div>
+                            <div class="lernMore">
+                                <button class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="mechanicalCards">
+                            <div class="cardimg">
+                                <img src="assets/imgaes/Rectangle 23796.png" alt="img" />
+                            </div>
+                            <div class="cardTitle">
+                                <h6>Mechanical</h6>
+                            </div>
+                            <div class="cardcontain">
+                                <ul>
+                                    <li>3D drafting drawings</li>
+                                    <li>General Layout</li>
+                                    <li>General Assembly</li>
+                                    <li>Detailed component drawing</li>
+                                    <li>Detailed fabrication drawing</li>
+                                    <li>Converting 3D to 2D drawing</li>
+                                    <li>Staging and Assembly drawings</li>
+                                </ul>
+                            </div>
+                            <div class="lernMore">
+                                <button class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-12">
+                        <div class="mechanicalCards">
+                            <div class="cardimg">
+                                <img src="assets/imgaes/Rectangle 23796.png" alt="img" />
+                            </div>
+                            <div class="cardTitle">
+                                <h6>Mechanical</h6>
+                            </div>
+                            <div class="cardcontain">
+                                <ul>
+                                    <li>3D drafting drawings</li>
+                                    <li>General Layout</li>
+                                    <li>General Assembly</li>
+                                    <li>Detailed component drawing</li>
+                                    <li>Detailed fabrication drawing</li>
+                                    <li>Converting 3D to 2D drawing</li>
+                                    <li>Staging and Assembly drawings</li>
+                                </ul>
+                            </div>
+                            <div class="lernMore">
+                                <button class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="DomainSection">
+    <div class="Cust_container">
+        <div class="aboutContain">
+            <div class="aboutTitle">
+                <h5>Our Domain Exposure</h5>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include "includes/footer.php"; ?>
--- includes/header.php
+++ includes/header.php
@@ -9,9 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assests-Eyes</title>
     <link href="assets/css/style.css" rel="stylesheet" type="">
+    <link href="assets/css/titas.css" rel="stylesheet" type="">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,500;1,600;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
@@ -20,24 +24,24 @@
 
 <section class="header">
     <div class="Cust_container">
-      <div class="row" style="align-items:center;">
-        <div class="col-lg-3 col-md-3 col-12">
-            <div class="logo">
-                <img src='assets/imgaes/logo.png' alt=''/>
+        <div class="row" style="align-items:center;">
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="logo">
+                    <img src='assets/imgaes/logo.png' alt='' />
+                </div>
+            </div>
+            <div class="col-lg-9 col-md-9 col-12">
+                <ul class="navMenu">
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='About.php'>About</a></li>
+                    <li><a href='HVAC.php'>Industry</a></li>
+                    <li><a href='#'>FAQs</a></li>
+                    <li><a href='#'>Blog</a></li>
+                    <li><a href='#'>Contact Us</a></li>
+
+                </ul>
             </div>
         </div>
-        <div class="col-lg-9 col-md-9 col-12">
-            <ul class="navMenu">
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Industry</a></li>
-                <li><a href='#'>FAQs</a></li>
-                <li><a href='#'>Blog</a></li>
-                <li><a href='#'>Contact Us</a></li>
-
-            </ul>
-        </div>
-      </div>
     </div>
 </section>
 
