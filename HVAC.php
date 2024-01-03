3506e1cfa95cac6abb1cd9f56ac8146a91336aac
titas2002 <titaspratihar@gmail.com>
2024-01-03 15:59:17 +0530

    up

--- HVAC.php
+++ HVAC.php
@@ -2,7 +2,7 @@
 
 <section class="aboutbanner">
     <div class="aboutBannerImg">
-        <img src="assets/imgaes/aboutBanner.jpg" alt="banner" />
+        <img src="assets/imgaes/hvacBanner1.webp" alt="banner" />
     </div>
     <div class="AboutbannerOverlay"></div>
     <div class="aboutBannerContain">
@@ -24,7 +24,7 @@
             </div>
             <div class="hvacSection">
                 <div class="row">
-                    <div class="col-lg-4 col-md-6 col-12">
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                         <div class="mechanicalCards">
                             <div class="cardimg">
                                 <img src="assets/imgaes/Rectangle 23796.png" alt="img" />
@@ -44,55 +44,55 @@
                                 </ul>
                             </div>
                             <div class="lernMore">
-                                <button class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></button>
+                                <a href="singleproductPage.php" class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 col-12">
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                         <div class="mechanicalCards">
                             <div class="cardimg">
-                                <img src="assets/imgaes/Rectangle 23796.png" alt="img" />
+                                <img src="assets/imgaes/Rectangle 23809.png" alt="img" />
                             </div>
                             <div class="cardTitle">
-                                <h6>Mechanical</h6>
+                                <h6>Electrical</h6>
                             </div>
                             <div class="cardcontain">
                                 <ul>
-                                    <li>3D drafting drawings</li>
-                                    <li>General Layout</li>
-                                    <li>General Assembly</li>
-                                    <li>Detailed component drawing</li>
-                                    <li>Detailed fabrication drawing</li>
-                                    <li>Converting 3D to 2D drawing</li>
-                                    <li>Staging and Assembly drawings</li>
+                                    <li>Electrical Design Services</li>
+                                    <li>BOM Generation based on preferred Brands</li>
+                                    <li>Schematic Electrical Drafting</li>
+                                    <li>CAD Electrical Drafting Layouts.</li>
+                                    <li>Electrical Engineering Power System Designs.</li>
+                                    <li>Power & Lighting Distribution Boards</li>
+                                    <li>Electrical Load Calculation.</li>
                                 </ul>
                             </div>
                             <div class="lernMore">
-                                <button class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></button>
+                                <a href="singleproductPage.php" class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-6 col-12">
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                         <div class="mechanicalCards">
                             <div class="cardimg">
-                                <img src="assets/imgaes/Rectangle 23796.png" alt="img" />
+                                <img src="assets/imgaes/Rectangle 23811.png" alt="img" />
                             </div>
                             <div class="cardTitle">
-                                <h6>Mechanical</h6>
+                                <h6>Automation</h6>
                             </div>
                             <div class="cardcontain">
                                 <ul>
-                                    <li>3D drafting drawings</li>
-                                    <li>General Layout</li>
-                                    <li>General Assembly</li>
-                                    <li>Detailed component drawing</li>
-                                    <li>Detailed fabrication drawing</li>
-                                    <li>Converting 3D to 2D drawing</li>
-                                    <li>Staging and Assembly drawings</li>
+                                    <li>I/O Listing</li>
+                                    <li>Control panel drawings with Local & Distributed Architectures</li>
+                                    <li>Control System BOM</li>
+                                    <li>Control Logic Flow Diagrams</li>
+                                    <li>Logic Development</li>
+                                    <li>HMI/ SCADA Screen Flow</li>
+                                    <li> HMI/ SCADA Screen Development</li>
                                 </ul>
                             </div>
                             <div class="lernMore">
-                                <button class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></button>
+                                <a href="singleproductPage.php" class="leunBtn">Learn more <i class="fa-solid fa-angle-right"></i></a>
                             </div>
                         </div>
                     </div>
@@ -104,9 +104,126 @@
 
 <section class="DomainSection">
     <div class="Cust_container">
-        <div class="aboutContain">
-            <div class="aboutTitle">
-                <h5>Our Domain Exposure</h5>
+        <div class="aboutTitle">
+            <h5>Our Domain Exposure</h5>
+        </div>
+        <div class="row">
+            <div class="col-lg-7 col-md-7 col-12">
+                <div class="domaincontainSection">
+                    <div class="domainTitle">
+                        <h5>Ventilation system</h5>
+                    </div>
+                    <div class="domainContain">
+                        <ul>
+                            <li>Based on the Customer and workplace need, capacity of evaporative cooling system can be calculated.</li>
+                            <li>Optimal velocity at the exit of the grill can be arrived based on the ventilation needs.</li>
+                            <li>Type of centrifugal fan can be selected keeping optimal operating energy in mind. </li>
+                            <li>The ventilation system what we design supports sustainability and reduced carbon footprint.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-5 col-12">
+                <div class="domainImg1">
+                    <img src="assets/imgaes/domain1.png" alt="img">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-7 col-md-7 col-12">
+                <div class="domaincontainSection">
+                    <div class="domainTitle">
+                        <h5>Comfort and Precision Air Conditioning system</h5>
+                    </div>
+                    <div class="domainContain">
+                        <ul>
+                            <li>Based on the Customer and workplace need, capacity of evaporative cooling system can be calculated.</li>
+                            <li>Optimal velocity at the exit of the grill can be arrived based on the ventilation needs.</li>
+                            <li>Type of centrifugal fan can be selected keeping optimal operating energy in mind. </li>
+                            <li>The ventilation system what we design supports sustainability and reduced carbon footprint.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-5 col-12">
+                <div class="domainImg">
+                    <img src="assets/imgaes/domain2.jpeg" alt="img">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-7 col-md-7 col-12">
+                <div class="domaincontainSection">
+                    <div class="domainTitle">
+                        <h5>Air Distribution system/Ducting system</h5>
+                    </div>
+                    <div class="domainContain">
+                        <ul>
+                            <li>Based on the Customer and workplace need, capacity of evaporative cooling system can be calculated.</li>
+                            <li>Optimal velocity at the exit of the grill can be arrived based on the ventilation needs.</li>
+                            <li>Type of centrifugal fan can be selected keeping optimal operating energy in mind. </li>
+                            <li>The ventilation system what we design supports sustainability and reduced carbon footprint.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-5 col-12">
+                <div class="domainImg">
+                    <img src="assets/imgaes/domain3.png" alt="img">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="hvacDtlsSection">
+    <div class="Cust_container">
+        <div class="hvacDtls">
+            <div class="hvacDtlsTitle">
+                <h6>What do we Offer?</h6>
+            </div>
+            <div class="hvacDtlsContain">
+                <p>Asset-Eyes Infomatics is driven by a clear mission: to be the single-stop shop where clients can confidently rely on us for their
+                    engineering drawing needs. Our aim is to provide exceptional attention to detail, create a secure and cyber-protected
+                    environment, and foster a sense of trust and collaboration with our clients. We understand the importance of each project and
+                    strive to deliver solutions that exceed expectations while adhering to the highest standards of quality and cybersecurity.</p>
+            </div>
+        </div>
+        <div class="hvacDtls">
+            <div class="hvacDtlsTitle">
+                <h6>Data Protection</h6>
+            </div>
+            <div class="hvacDtlsContain">
+                <p>We recognize the critical nature of safeguarding our clients' intellectual property and sensitive data. At Asset-Eyes Infomatics,
+                    we prioritize your information security and have implemented robust measures to ensure the utmost protection of client
+                    information. Our secure infrastructure, stringent data access controls, and continuous monitoring protocols ensure that our
+                    clients can confidently share their product information with us, knowing it is handled with the highest level of confidentiality
+                    and integrity.</p>
+            </div>
+        </div>
+        <div class="hvacDtls">
+            <div class="hvacDtlsTitle">
+                <h6>Key Takeaways</h6>
+            </div>
+            <div class="hvacDtlsContain">
+                <ul>
+                    <li>Holistic solution to your Engineering Design requirements panning across Mechanical, Electrical & Automation fields.
+                    </li>
+                    <li>A reliable partner for top-notch engineering drawing services, specializing in 3D software such as SolidWorks, AutoDesk &
+                        Revit and AutoCAD.
+                    </li>
+                    <li>
+                        Dedicated to delivering precision, attention to detail, and a secure environment where you can trust us with your valuable
+                        designs.
+                    </li>
+                    <li>
+                        Comprehensive range of services and talented professionals empower businesses holistically.
+
+                    </li>
+                    <li>Partnership experience fostering confidence and peace of mind, that comes with having a trusted ally in the realm of
+                        engineering drawing services.
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
---
+++ singleproductPage.php
@@ -0,0 +1,98 @@
+<?php include "includes/header.php"; ?>
+<section class="aboutbanner">
+    <div class="aboutBannerImg">
+        <img src="assets/imgaes/MechanicalBannerimg.avif" alt="banner" />
+    </div>
+    <div class="AboutbannerOverlay"></div>
+    <div class="aboutBannerContain">
+        <div class="bannerTitle">
+            <h2>Single Product</h2>
+        </div>
+        <div class="Anoutbannerlink">
+            <a href="HVAC.php">HVAC <i class="fa-solid fa-angle-right"></i></a>
+            <a href="singleproductPage.php">Mechanical</a>
+        </div>
+    </div>
+</section>
+
+<section class="areaSection">
+    <div class="Cust_container">
+        <div class="areaTitle">
+            <h3>Area of Expertise</h3>
+        </div>
+        <div class="areaContainSection">
+            <div class="areaContainLeft">
+                <div class="areaDtls">
+                    <p>Functional Skills</p>
+                </div>
+                <div class="areaContain">
+                    <p>Sheet Metal</p>
+                </div>
+                <div class="areaContain">
+                    <p>Weldings</p>
+                </div>
+                <div class="areaContain">
+                    <p>Refrigeration System</p>
+                </div>
+                <div class="areaContain">
+                    <p>CO2 Trans-Critical System</p>
+                </div>
+                <div class="areaContain">
+                    <p>Reverse Engineering</p>
+                </div>
+                <div class="areaContain">
+                    <p>GD & T</p>
+                </div>
+                <div class="areaContain">
+                    <p>FEA / FMEA</p>
+                </div>
+            </div>
+            <div class="areaContainRight">
+                <div class="areaDtls">
+                    <p>Software Skills</p>
+                </div>
+                <div class="areaContain">
+                    <p>SolidWorks</p>
+                </div>
+                <div class="areaContain">
+                    <p>AutoCAD</p>
+                </div>
+                <div class="areaContain">
+                    <p>Inventor</p>
+                </div>
+                <div class="areaContain">
+                    <p>Catia V5</p>
+                </div>
+                <div class="areaContain">
+                    <p>Creo</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ProductDtlsSection">
+    <div class="Cust_container">
+        <div class="productNameTitle">
+            <h3>Chiller System</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-7 col-md-7 col-12">
+                <div class="domaincontainSection">
+                    <!-- <div class="domainTitle">
+                        <h5>Ventilation system</h5>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+<?php include "includes/footer.php"; ?>
