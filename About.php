402c1a0e1afe863cc26a3449b8aa16518e3d31f5
jhumdas <dasjhuma0099@gmail.com>
2024-01-18 15:00:41 +0530

    up

--- About.php
+++ About.php
@@ -33,5 +33,58 @@
         </div>
     </div>
 </section>
-
+<div class="mobileUnderLine mb-5 mt-0"></div>
+<section class="aboutauthorSection">
+    <div class="Cust_container">
+            <div class="">
+                <div class="aboutContain">
+              <div class="aboutTitle">
+                    <h5>Business Enablers</h5>
+              </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12 authorCardborder">
+                    <div class="authorCardSection">
+                        <div class="authorHeaderSection">
+                            <div class="authorImg">
+                                <img src="assets/imgaes/image.png" alt="img">
+                            </div>
+                            <div class="authorTitle">
+                                <h5>Rajesh Raman</h5>
+                                <h6>VP Business</h6>
+                            </div>
+                        </div>
+                        <div class="authorContain">
+                            <ul>
+                                <li>P&amp;L, Business Development, Technology, Projects Management</li>
+                                <li>Industrial Automation, IoT  ioajdank ajsihsduih jauihuasgd aughuygadugb agyugayu</li>
+                                <li>Rockwell Automation, Serial Entrepreneur, Educationist</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-lg-6 col-md-6 col-12 authorCardborder">
+                    <div class="authorCardSection">
+                        <div class="authorHeaderSection">
+                            <div class="authorImg">
+                                <img src="assets/imgaes/image (1).png" alt="img">
+                            </div>
+                            <div class="authorTitle">
+                                <h5>Siva S Mantha</h5>
+                                <h6>VP Operations</h6>
+                            </div>
+                        </div>
+                        <div class="authorContain">
+                            <ul>
+                                <li>P&amp;L, Business Operations, Projects &amp; Services </li>
+                                <li>Electrical Systems ~ LV, MV, EHV  ahusg ayuguyd aghsugau sgdus aji</li>
+                                <li>Siemens, Schneider, Honeywell &amp; General Electric</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include "includes/footer.php"; ?>
--- FAQ.php
+++ FAQ.php
@@ -10,74 +10,197 @@
         <div class="bannerTitle">
             <h2>FAQs</h2>
         </div>
+    </div>
+</section>
+
+<section class="FAQbradecome">
+    <div class="Cust_container">
         <div class="Anoutbannerlink">
-            <a href="home.php">Home <i class="fa-solid fa-angle-right"></i> </a>
-            <a href="FAQ.php">FAQs</a>
+            <a href="index.php"> <i class="fa-solid fa-chevron-left"></i> Home <i class="fa-solid fa-angles-right"></i> </a>
+            <a href="FAQ.php" class="active">FAQs</a>
         </div>
     </div>
 </section>
 
-<section class="faqSection">
+<section class="faqSection pb-0">
     <div class="Cust_container">
         <div class="aboutContain" data-aos="zoom-in" data-aos-duration="3000">
             <div class="faqTitle">
                 <h5>FAQs</h5>
             </div>
 
-            <div class="accodian Section">
-                <div class="accs">
-                    <div class="acc-item">
-                        <h3>
-                            <span>This is a question</span>
-                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>
-                        </h3>
-                        <p>
-                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.
-                            <button class="accsBtn">Learn more</button>
-                        </p>
-                    </div>
-                    <div class="acc-item">
-                        <h3>
-                            <span>This is a question</span>
-                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>
-                        </h3>
-                        <p>
-                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.
-                            <button class="accsBtn">Learn more</button>
-                        </p>
-                    </div>
-                    <div class="acc-item">
-                        <h3>
-                            <span>This is a question</span>
-                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>
-                        </h3>
-                        <p>
-                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.
-                            <button class="accsBtn">Learn more</button>
-                        </p>
-                    </div>
-                    <div class="acc-item">
-                        <h3>
-                            <span>This is a question</span>
-                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>
-                        </h3>
-                        <p>
-                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.
-                            <button class="accsBtn">Learn more</button>
-                        </p>
-                    </div>
-                    <div class="acc-item">
-                        <h3>
-                            <span>This is a question</span>
-                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>
-                        </h3>
-                        <p>
-                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.
+            <div class="accodianSection">
+                <div id="main">
+                  <div class="">
+                <!--    <div class="accordion" id="faq">-->
+                <!--    <div class="card">-->
+                <!--        <div class="card-header" id="faqhead1">-->
+                <!--            <a href="#" class="btn btn-header-link" data-toggle="collapse" data-target="#faq1"-->
+                <!--            aria-expanded="true" aria-controls="faq1">This is a question</a>-->
+                <!--        </div>-->
 
-                        </p>
-                        <button class="accsBtn">Learn more</button>
-                    </div>
-                </div>
+                <!--        <div id="faq1" class="collapse show" aria-labelledby="faqhead1" data-parent="#faq">-->
+                <!--            <div class="card-body">-->
+                <!--               This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                <!--            </div>-->
+                <!--        </div>-->
+                <!--    </div>-->
+                <!--    <div class="card">-->
+                <!--        <div class="card-header" id="faqhead2">-->
+                <!--            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq2"-->
+                <!--            aria-expanded="true" aria-controls="faq2">This is a question</a>-->
+                <!--        </div>-->
+
+                <!--        <div id="faq2" class="collapse" aria-labelledby="faqhead2" data-parent="#faq">-->
+                <!--            <div class="card-body">-->
+                <!--               This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                <!--            </div>-->
+                <!--        </div>-->
+                <!--    </div>-->
+                <!--      <div class="card">-->
+                <!--        <div class="card-header" id="faqhead3">-->
+                <!--            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq3"-->
+                <!--            aria-expanded="true" aria-controls="faq3">This is a question</a>-->
+                <!--        </div>-->
+
+                <!--        <div id="faq3" class="collapse" aria-labelledby="faqhead3" data-parent="#faq">-->
+                <!--            <div class="card-body">-->
+                <!--               This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                <!--            </div>-->
+                <!--        </div>-->
+                <!--    </div>-->
+                <!--      <div class="card">-->
+                <!--        <div class="card-header" id="faqhead4">-->
+                <!--            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq4"-->
+                <!--            aria-expanded="true" aria-controls="faq4">This is a question</a>-->
+                <!--        </div>-->
+
+                <!--        <div id="faq4" class="collapse" aria-labelledby="faqhead4" data-parent="#faq">-->
+                <!--            <div class="card-body">-->
+                <!--               This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                <!--            </div>-->
+                <!--        </div>-->
+                <!--    </div>-->
+                <!--      <div class="card">-->
+                <!--        <div class="card-header" id="faqhead5">-->
+                <!--            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq5"-->
+                <!--            aria-expanded="true" aria-controls="faq5">This is a question</a>-->
+                <!--        </div>-->
+
+                <!--        <div id="faq5" class="collapse" aria-labelledby="faqhead5" data-parent="#faq">-->
+                <!--            <div class="card-body">-->
+                <!--               This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                <!--            </div>-->
+                <!--        </div>-->
+                <!--    </div>-->
+                <!--    <div class="card">-->
+                <!--        <div class="card-header" id="faqhead6">-->
+                <!--            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq6"-->
+                <!--            aria-expanded="true" aria-controls="faq6">This is a question</a>-->
+                <!--        </div>-->
+
+                <!--        <div id="faq6" class="collapse" aria-labelledby="faqhead6" data-parent="#faq">-->
+                <!--            <div class="card-body">-->
+                <!--               This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                <!--            </div>-->
+                <!--        </div>-->
+                <!--    </div>-->
+                <!--</div>-->
+                
+                <div class="accordion-container">
+    <div class="accordion">
+        <h3>This is a question</h3>
+        <div class="accordion-content">
+            <p>This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.</p>
+        </div>
+    </div>
+    <div class="accordion">
+        <h3>This is a question</h3>
+        <div class="accordion-content">
+            <p>This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.</p>
+        </div>
+    </div>
+    <div class="accordion">
+        <h3>This is a question</h3>
+        <div class="accordion-content">
+            <p>This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.</p>
+        </div>
+    </div>
+    <div class="accordion">
+        <h3>This is a question</h3>
+        <div class="accordion-content">
+            <p>This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.</p>
+        </div>
+    </div>
+    <div class="accordion">
+        <h3>This is a question</h3>
+        <div class="accordion-content">
+            <p>This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.</p>
+        </div>
+    </div>
+</div>
+    </div>
+  </div>
+              
+                
+                
+                
+                
+                
+                
+<!--<div class="accs">-->
+<!--                    <div class="acc-item">-->
+<!--                        <h3>-->
+<!--                            <span>This is a question</span>-->
+<!--                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>-->
+<!--                        </h3>-->
+<!--                        <p>-->
+<!--                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                            <!--<button class="accsBtn">Learn more</button>-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                    <div class="acc-item">-->
+<!--                        <h3>-->
+<!--                            <span>This is a question</span>-->
+<!--                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>-->
+<!--                        </h3>-->
+<!--                        <p>-->
+<!--                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                            <!--<button class="accsBtn">Learn more</button>-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                    <div class="acc-item">-->
+<!--                        <h3>-->
+<!--                            <span>This is a question</span>-->
+<!--                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>-->
+<!--                        </h3>-->
+<!--                        <p>-->
+<!--                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                            <!--<button class="accsBtn">Learn more</button>-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                    <div class="acc-item">-->
+<!--                        <h3>-->
+<!--                            <span>This is a question</span>-->
+<!--                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>-->
+<!--                        </h3>-->
+<!--                        <p>-->
+<!--                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+                            <!--<button class="accsBtn">Learn more</button>-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                    <div class="acc-item">-->
+<!--                        <h3>-->
+<!--                            <span>This is a question</span>-->
+<!--                            <img class="acc-chevron" src="https://svgshare.com/i/zQr.svg"></img>-->
+<!--                        </h3>-->
+<!--                        <p>-->
+<!--                            This is just a dummy text that has been inserted as a placeholder for future content. While it may seem insignificant at first glance, the use of dummy text is a common practice in the design and publishing industry, as it allows designers and developers to visualize the layout and overall aesthetic of a project without being distracted by the actual content.-->
+  <!--<button class="accsBtn">Learn more</button>-->
+<!--                        </p>-->
+                      
+<!--                    </div>-->
+<!--                </div>-->
             </div>
         </div>
     </div>
@@ -143,3 +266,4 @@
 
 
 <?php include "includes/footer.php"; ?>
+
